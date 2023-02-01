bb9bb863713737738ea00b37b03a260b8d32ce96
Felipe Augusto Lara Soares <64044758+felipealsoares@users.noreply.github.com>
2023-02-01 01:16:11 -0300

    Add files via upload

---
+++ scripts/ApplicationCreateDatabase.py
@@ -0,0 +1,8 @@
+
+
+
+from controllers import ControllerCreateDatabase
+
+
+if __name__ == "__main__":
+    ControllerCreateDatabase.main()
---
+++ scripts/ApplicationPrediction.py
@@ -0,0 +1,8 @@
+
+
+
+from processing import ControllerProcessing
+
+
+if __name__ == "__main__":
+    ControllerProcessing.main()
